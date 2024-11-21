bd28be29dd48879f79aee84062e0094a4f8a89f5
Manuel <87156289+pertinaz@users.noreply.github.com>
2024-11-21 16:22:08 -0500

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,9 @@
+-- INSERCION DE DATOS (INSERT QUERY)
+
+-- TABLAS INDEPENDIENTES
+INSERT INTO marcas (nombre_marca, direccion_concesionario) 
+VALUES ('Toyota','Av. Principal 332'),
+			 ('Honda','Calle Estrella 221'),
+			 ('Kia','Municipal Horiente 322'),
+			 ('Chevrolet','Parque central 112'),
+			 ('BMW','Alamos sur 202');
