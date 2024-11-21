4e38a1e869e4025ad848b2aaf2c96dad462025ef
Manuel <87156289+pertinaz@users.noreply.github.com>
2024-11-21 16:28:36 -0500

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -7,3 +7,11 @@ VALUES ('Toyota','Av. Principal 332'),
 			 ('Kia','Municipal Horiente 322'),
 			 ('Chevrolet','Parque central 112'),
 			 ('BMW','Alamos sur 202');
+
+INSERT INTO propietarios (apellidos,nombres,nacimiento,domicilio) 
+VALUES ('Gómez', 'Juan', '1985-06-15', 'Pereira, Risaralda'),
+			 ('Agudelo', 'Diana', '1995-01-12', 'Medellín, Antioquia'),
+			 ('Arias', 'Cristina', '1999-08-13', 'Armenia, Quindio'),
+			 ('Aguirre', 'Julio', '1973-10-11', 'Cali, Valle del Cauca'),
+			 ('Ortiz', 'Mario', '1989-12-06', 'Bogotá, Cundinamarca');
+		
